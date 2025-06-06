d834414e41e5345a111f71dd82275007c43ba9bc
clara <claraafurinii@gmail.com>
2025-06-05 22:07:15 -0300

    Test_doc

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,10 @@
 const logger = require('../logger');
 
 function errorHandler(err, req, res, next) {
-  // Loga o erro com stack trace
+
   logger.error(err.message, { stack: err.stack });
 
-  // Retorna o status apropriado
+  
   const status = err.status || 500;
   res.status(status).json({ error: err.message || 'Erro interno do servidor' });
 }
